801a9a6791b48db4a25dcfce7d7e343f31cc7d2f
Pablo Torrez <pablotorrez90@outlook.com>
2024-06-03 16:55:51 -0300

    ultimando detalles del sistema

--- assets/js/carrito.mjs
+++ assets/js/carrito.mjs
@@ -2,8 +2,9 @@
 import { miContenedorProductosAgregadosCarrito } from "./main.mjs";
 
 const d = document;
-const msg_carrito = d.querySelector(".msg-carrito");
-let txt_total_productos = d.getElementById("total");
+const $msg_carrito = d.querySelector(".msg-carrito");
+
+let $txt_total_productos = d.getElementById("total");
 let total = 0;
 let productos_agregados_count = 0;
 let productosAgregados = {};
@@ -14,7 +15,6 @@ export const products_json_carrito = () => {
     fetch('assets/js/productos.json')
       .then(response => (response.ok ? response.json() : Promise.reject(`HTTP error! Status: ${response.status}`)))
       .then(data => {
-
         inicializarEventos(data);
       })
       .catch(error => console.error('No ha sido posible agregar productos al carrito', error));
@@ -23,13 +23,14 @@ export const products_json_carrito = () => {
 
 // Inicializar los eventos de los botones
 const inicializarEventos = (data) => {
-  let cantidadInfo = d.querySelectorAll(".btn-cantidades");
-  const botones_carrito = d.querySelectorAll(".btn-agregar-carrito");
-  const boton_comprar = d.getElementById("btn-finalizar-compra");
-
+  let $cantidadInfo = d.querySelectorAll(".btn-cantidades");
   let clickContador = 0;
-  if (botones_carrito.length > 0 && botones_carrito.length === cantidadInfo.length) {
-    botones_carrito.forEach((boton, index) => {
+
+  const $botones_carrito = d.querySelectorAll(".btn-agregar-carrito");
+  const $boton_comprar = d.getElementById("btn-finalizar-compra");
+
+  if ($botones_carrito.length > 0 && $botones_carrito.length === $cantidadInfo.length) {
+    $botones_carrito.forEach((boton, index) => {
       boton.addEventListener('click', () => {
         clickContador++;
         console.log(`El usuario ha hecho click ${clickContador} veces`);
@@ -40,7 +41,7 @@ const inicializarEventos = (data) => {
     console.warn("La NodeList de botones para agregar al carrito está vacía o no coincide con las cantidades.");
   }
 
-  boton_comprar.addEventListener("click", (e) => {
+  $boton_comprar.addEventListener("click", (e) => {
     finalizarCompra(e);
   });
 };
@@ -54,25 +55,19 @@ const agregarAlCarrito = (data, boton, index) => {
     }
 
     if (producto.cantidad > 0) {
+      
       producto.cantidad--;
-
-      let productosCarrito = d.getElementById(`id-prod-carrito-${index}`);
-      if (productosCarrito) {
-        let cantidadSpanProductos = productosCarrito.querySelector('.cantidad-producto-carrito');
-        cantidadSpanProductos.textContent = parseInt(cantidadSpanProductos.textContent) + 1;
+      let $productosCarrito = d.getElementById(`id-prod-carrito-${index}`);
+      if ($productosCarrito) {
+        let $cantidadSpanProductos = $productosCarrito.querySelector('.cantidad-producto-carrito');
+        $cantidadSpanProductos.textContent = parseInt($cantidadSpanProductos.textContent) + 1;
       } else {
-
         crearElementoProductoCarrito(producto, index);
-
-        
       }
 
       actualizarTotalYContador(producto);
 
-      
-
       console.info(`Agregado al carrito: \nID del Botón: '${boton.id}',\nNombre Producto: '${producto.producto}',\nÍndice: [${index}],\nCantidad Disponible Original: '${producto.cantidad + 1}',\nCantidad Disponible Actual: '${producto.cantidad}'`);
-
     } else {
       console.warn(`Ya no quedan productos disponibles de ${producto.producto}`);
       const showAlert = () => {
@@ -93,59 +88,60 @@ const agregarAlCarrito = (data, boton, index) => {
 
 // Crea el elemento del producto en el carrito
 const crearElementoProductoCarrito = (producto, index) => {
-  let contenido_producto_carrito = d.createElement('div');
-  contenido_producto_carrito.classList.add('productos-carrito');
-  contenido_producto_carrito.id = `id-prod-carrito-${index}`;
-  miContenedorProductosAgregadosCarrito.appendChild(contenido_producto_carrito);
+  let $contenido_producto_carrito = d.createElement('div');
+  $contenido_producto_carrito.classList.add('productos-carrito');
+  $contenido_producto_carrito.id = `id-prod-carrito-${index}`;
+  miContenedorProductosAgregadosCarrito.appendChild($contenido_producto_carrito);
 
-  let cantidad_productos_agregados_span = d.createElement('span');
-  cantidad_productos_agregados_span.classList.add('cantidad-producto-carrito');
-  cantidad_productos_agregados_span.textContent = '1';
-  contenido_producto_carrito.appendChild(cantidad_productos_agregados_span);
+  let $cantidad_productos_agregados_span = d.createElement('span');
+  $cantidad_productos_agregados_span.classList.add('cantidad-producto-carrito');
+  $cantidad_productos_agregados_span.textContent = '1';
+  $contenido_producto_carrito.appendChild($cantidad_productos_agregados_span);
 
-  let contenedor_imagen_producto = d.createElement("div");
-  contenedor_imagen_producto.classList.add('div-img-carrito', 'prod-carr');
-  contenedor_imagen_producto.id = "id-img-carrito";
-  contenido_producto_carrito.appendChild(contenedor_imagen_producto);
+  let $contenedor_imagen_producto = d.createElement("div");
+  $contenedor_imagen_producto.classList.add('div-img-carrito', 'prod-carr');
+  $contenedor_imagen_producto.id = "id-img-carrito";
+  $contenido_producto_carrito.appendChild($contenedor_imagen_producto);
 
-  let imagen_producto_carrito = d.createElement("img");
-  imagen_producto_carrito.src = producto.url;
-  imagen_producto_carrito.alt = producto.producto;
-  contenedor_imagen_producto.appendChild(imagen_producto_carrito);
+  let $imagen_producto_carrito = d.createElement("img");
+  $imagen_producto_carrito.src = producto.url;
+  $imagen_producto_carrito.alt = producto.producto;
+  $contenedor_imagen_producto.appendChild($imagen_producto_carrito);
 
-  let contenedor_info_carrito = d.createElement("div");
-  contenedor_info_carrito.classList.add('div-info-carrito', 'prod-carr');
-  contenedor_info_carrito.id = "id-info-carrito";
-  contenido_producto_carrito.appendChild(contenedor_info_carrito);
+  let $contenedor_info_carrito = d.createElement("div");
+  $contenedor_info_carrito.classList.add('div-info-carrito', 'prod-carr');
+  $contenedor_info_carrito.id = "id-info-carrito";
+  $contenido_producto_carrito.appendChild($contenedor_info_carrito);
 
-  let contenedor_nombre_precio_producto = d.createElement("div");
-  contenedor_nombre_precio_producto.classList.add('div-nombre-precio-producto');
-  contenedor_nombre_precio_producto.id = "id-nombre-precio-producto";
-  contenedor_nombre_precio_producto.innerHTML = `
+  let $contenedor_nombre_precio_producto = d.createElement("div");
+  $contenedor_nombre_precio_producto.classList.add('div-nombre-precio-producto');
+  $contenedor_nombre_precio_producto.id = "id-nombre-precio-producto";
+  $contenedor_nombre_precio_producto.innerHTML = `
     <span class="span-nombre">${producto.producto}</span>
     <span class="span-precio">$${producto.precio}</span>
   `;
-  contenedor_info_carrito.appendChild(contenedor_nombre_precio_producto);
+  $contenedor_info_carrito.appendChild($contenedor_nombre_precio_producto);
 
-  let contenedor_btn_eliminar_producto_carrito = d.createElement("div");
-  contenedor_btn_eliminar_producto_carrito.classList.add('div-btn-eliminar-producto-carrito');
-  contenedor_btn_eliminar_producto_carrito.id = "id-btn-eliminar-producto-carrito";
-  contenedor_info_carrito.appendChild(contenedor_btn_eliminar_producto_carrito);
+  let $contenedor_btn_eliminar_producto_carrito = d.createElement("div");
+  $contenedor_btn_eliminar_producto_carrito.classList.add('div-btn-eliminar-producto-carrito');
+  $contenedor_btn_eliminar_producto_carrito.id = "id-btn-eliminar-producto-carrito";
+  $contenedor_info_carrito.appendChild($contenedor_btn_eliminar_producto_carrito);
 
-  let boton_remover_producto = d.createElement("button");
-  boton_remover_producto.classList.add("btn-remover-producto");
-  boton_remover_producto.onclick = () => {
+  let $boton_remover_producto = d.createElement("button");
+  $boton_remover_producto.classList.add("btn-remover-producto");
+  $boton_remover_producto.onclick = () => {
     eliminar_producto_carrito(producto, index);
   }
-  boton_remover_producto.innerHTML = `
+  $boton_remover_producto.innerHTML = `
     <i class="fas fa-trash" style="color:red";></i>
   `;
-  contenedor_btn_eliminar_producto_carrito.appendChild(boton_remover_producto);
+  $contenedor_btn_eliminar_producto_carrito.appendChild($boton_remover_producto);
 }
 
 const actualizarTotalYContador = (producto) => {
   let precio_producto = parseInt(`${producto.precio}`);
   total += precio_producto;
+  
   actualizarTotal();
 
   productosAgregados[producto.id]++;
@@ -161,7 +157,7 @@ const numero_productos_agregados = () => {
 
 // Actualiza el total del carrito
 const actualizarTotal = () => {
-  txt_total_productos.innerText = `Total: $${total}`;
+  $txt_total_productos.innerText = `Total: $${total}`;
 };
 
 // Elimina un producto del carrito
@@ -171,18 +167,20 @@ const eliminar_producto_carrito = (producto, index) => {
   
   let precioEliminado = productoEliminado.precio;
   total -= precioEliminado;
+  
   productos_agregados_count--;
+  
   numero_productos_agregados();
   actualizarTotal();
 
-  let productoCarrito = d.getElementById(`id-prod-carrito-${index}`);
-  if (productoCarrito) {
-    let cantidadSpan = productoCarrito.querySelector('.cantidad-producto-carrito');
-    let cantidadActual = parseInt(cantidadSpan.textContent);
+  let $productoCarrito = d.getElementById(`id-prod-carrito-${index}`);
+  if ($productoCarrito) {
+    let $cantidadSpan = $productoCarrito.querySelector('.cantidad-producto-carrito');
+    let cantidadActual = parseInt($cantidadSpan.textContent);
     if (cantidadActual > 1) {
-      cantidadSpan.textContent = cantidadActual - 1;
+      $cantidadSpan.textContent = cantidadActual - 1;
     } else {
-      productoCarrito.remove();
+      $productoCarrito.remove();
     }
   }
 
@@ -204,7 +202,7 @@ const finalizarCompra = (e) => {
   showAlert();
 
   total = 0;
-  txt_total_productos.innerText = `Total: $${total}`;
+  $txt_total_productos.innerText = `Total: $${total}`;
 
   const $numeroIncrementador = d.getElementById("numero-prod-carrito");
   $numeroIncrementador.textContent = productos_agregados_count = 0;
@@ -214,7 +212,7 @@ const finalizarCompra = (e) => {
     producto.remove();
   });
 
-  if ($productosEnCarrito.length === 0) msg_carrito.style.display = "block";
+  if ($productosEnCarrito.length === 0) $msg_carrito.style.display = "block";
 
   e.preventDefault();
 }     
--- assets/js/fav.mjs
+++ assets/js/fav.mjs
@@ -1,131 +1,137 @@
-// Para usar los import y export, a los archivos deben terminar con `.mjs`
+
 import { miContenedorProductosAgregadosFavoritos } from "./main.mjs";
 
 const d = document;
+const $msg_favoritos = d.querySelector(".msg-favoritos");
+let productos_favoritos_count = 0;
 
-// En esta función hago uso de API Fetch para obtener datos de mi JSON
+// Función para inicializar la carga del JSON y establcer los eventos
 export const products_json_favoritos = () => {
   d.addEventListener('DOMContentLoaded', () => {
     fetch('assets/js/productos.json')
       .then(response => (response.ok ? response.json() : Promise.reject(`HTTP error! Status: ${response.status}`)))
       .then(data => {
-        console.log('Datos de la API Country para Favoritos: ', data);
-
-        const botones_favoritos = d.querySelectorAll(".btn-agregar-favorito");
-        const msg_favoritos = d.querySelector(".msg-favoritos");
-        let productos_favoritos_count = 0; // Incrementa en el ícono los productos agregados.
-
-        if (botones_favoritos.length > 0) {
-          botones_favoritos.forEach((boton, index) => {
-            boton.addEventListener('click', () => {
-              console.log(`Botón de agregar a favoritos clickeado: ${boton.id}`);
-              agregarAFavoritos(boton, index);
-            });
-          });
-        } else {
-          console.warn("La NodeList de botones para agregar a favoritos está vacía.");
-        }
-
-        const agregarAFavoritos = (boton, index) => {
-          const producto = data[index];
-          if (producto) {
-            // Verificar si el producto ya está en favoritos
-            let productoFavorito = d.getElementById(`id-prod-favorito-${index}`);
-            if (productoFavorito) {
-              // Mostrar una advertencia si el producto ya está en favoritos
-              Swal.fire({
-                title: 'Advertencia',
-                text: 'Este producto ya está en favoritos.',
-                icon: 'warning',
-                confirmButtonText: 'Ok'
-              });
-            } else {
-              // Crear una nueva entrada para el producto en favoritos
-              let contenido_producto_favorito = d.createElement('div');
-              contenido_producto_favorito.classList.add('productos-favoritos');
-              contenido_producto_favorito.id = `id-prod-favorito-${index}`;
-              miContenedorProductosAgregadosFavoritos.appendChild(contenido_producto_favorito);
-
-              let cantidad_productos_favoritos_span = d.createElement('span');
-              cantidad_productos_favoritos_span.classList.add('cantidad-producto-favorito');
-              cantidad_productos_favoritos_span.textContent = '1';
-              contenido_producto_favorito.appendChild(cantidad_productos_favoritos_span);
-
-              let contenedor_imagen_producto = d.createElement("div");
-              contenedor_imagen_producto.classList.add('div-img-favoritos', 'prod-fav');
-              contenedor_imagen_producto.id = "id-img-favoritos";
-              contenido_producto_favorito.appendChild(contenedor_imagen_producto);
-
-              let imagen_producto_favorito = d.createElement("img");
-              imagen_producto_favorito.src = producto.url;
-              imagen_producto_favorito.alt = producto.producto;
-              contenedor_imagen_producto.appendChild(imagen_producto_favorito);
-
-              let contenedor_info_favorito = d.createElement("div");
-              contenedor_info_favorito.classList.add('div-info-favoritos', 'prod-fav');
-              contenedor_info_favorito.id = "id-info-favoritos";
-              contenido_producto_favorito.appendChild(contenedor_info_favorito);
-
-              let contenedor_nombre_precio_producto = d.createElement("div");
-              contenedor_nombre_precio_producto.classList.add('div-nombre-precio-producto');
-              contenedor_nombre_precio_producto.id = "id-nombre-precio-producto";
-              contenedor_nombre_precio_producto.innerHTML = `
-                <span class="span-nombre">${producto.producto}</span>
-                <span class="span-precio">$${producto.precio}</span>
-              `;
-              contenedor_info_favorito.appendChild(contenedor_nombre_precio_producto);
-
-              let contenedor_btn_eliminar_producto_favorito = d.createElement("div");
-              contenedor_btn_eliminar_producto_favorito.classList.add('div-btn-eliminar-producto-favoritos');
-              contenedor_btn_eliminar_producto_favorito.id = "id-btn-eliminar-producto-favoritos";
-              contenedor_info_favorito.appendChild(contenedor_btn_eliminar_producto_favorito);
-
-              let boton_remover_producto = d.createElement("button");
-              boton_remover_producto.classList.add("btn-remover-producto");
-              boton_remover_producto.onclick = () => {
-                eliminar_producto_favorito(index);
-              }
-              boton_remover_producto.innerHTML = `
-                <i class="fas fa-trash" style="color:red";></i>
-              `;
-              contenedor_btn_eliminar_producto_favorito.appendChild(boton_remover_producto);
-
-              console.info(`Agregado a favoritos: \nID del Botón: '${boton.id}',\nNombre Producto: '${producto.producto}',\nÍndice: [${index}]`);
-
-              productos_favoritos_count++;
-              numero_productos_favoritos();
-            }
-          } else {
-            console.warn(`No se encontró el producto correspondiente al índice [${index}]`);
-          }
-        };
-
-        const numero_productos_favoritos = () => {
-          let $numeroIncrementador = d.getElementById("numero-prod-favoritos");
-          if ($numeroIncrementador) {
-            $numeroIncrementador.textContent = productos_favoritos_count;
-          } else {
-            console.warn("Elemento de número de productos favoritos no encontrado.");
-          }
-        };
-
-        const eliminar_producto_favorito = (index) => {
-          const productoFavorito = d.getElementById(`id-prod-favorito-${index}`);
-          if (productoFavorito) {
-            productoFavorito.remove();
-
-            productos_favoritos_count--;
-            numero_productos_favoritos();
-
-            console.info(`Se ha eliminado de favoritos el Producto con índice [${index}]`);
-          } else {
-            console.warn(`No ha sido posible eliminar el producto con índice [${index}] de favoritos`);
-          }
-        };
-
+        inicializarEventos(data);
       })
       .catch(error => console.error('No ha sido posible agregar productos a los Favoritos', error));
   });
 }
+
+// Inicializar los eventos de los botones de agregar a favoritos
+const inicializarEventos = (data) => {
+  const $botones_favoritos = d.querySelectorAll(".btn-agregar-favorito");
+  if ($botones_favoritos.length > 0) {
+    $botones_favoritos.forEach((boton, index) => {
+      boton.addEventListener('click', () => {
+        console.log(`Botón de agregar a favoritos clickeado: ${boton.id}`);
+        agregarAFavoritos(data, boton, index);
+      });
+    });
+  } else {
+    console.warn("La NodeList de botones para agregar a favoritos está vacía.");
+  }
+}
+
+// Agregar un producto a favoritos
+const agregarAFavoritos = (data,boton, index) => {
+  const producto = data[index];
+  if (producto) {
+    let $productoFavorito = d.getElementById(`id-prod-favorito-${index}`);
+    if ($productoFavorito) {
+      Swal.fire({
+        title: 'Advertencia',
+        text: 'Este producto ya está en favoritos.',
+        icon: 'warning',
+        confirmButtonText: 'Ok'
+      });
+    } else {
+      crearElementoProductoFavorito(producto, index);
+
+      console.info(`Agregado a favoritos: \nID del Botón: '${boton.id}',\nNombre Producto: '${producto.producto}',\nÍndice: [${index}]`);
+
+      productos_favoritos_count++;
+      numero_productos_favoritos();
+    }
+  } else {
+    console.warn(`No se encontró el producto correspondiente al índice [${index}]`);
+  }
+};
+
+// Crea el elemento del producto en favoritos
+const crearElementoProductoFavorito = (producto, index) => {
+  let $contenido_producto_favorito = d.createElement('div');
+  $contenido_producto_favorito.classList.add('productos-favoritos');
+  $contenido_producto_favorito.id = `id-prod-favorito-${index}`;
+  miContenedorProductosAgregadosFavoritos.appendChild($contenido_producto_favorito);
+
+  let $cantidad_productos_favoritos_span = d.createElement('span');
+  $cantidad_productos_favoritos_span.classList.add('cantidad-producto-favorito');
+  $cantidad_productos_favoritos_span.textContent = '1';
+  $contenido_producto_favorito.appendChild($cantidad_productos_favoritos_span);
+
+  let $contenedor_imagen_producto = d.createElement("div");
+  $contenedor_imagen_producto.classList.add('div-img-favoritos', 'prod-fav');
+  $contenedor_imagen_producto.id = "id-img-favoritos";
+  $contenido_producto_favorito.appendChild($contenedor_imagen_producto);
+
+  let $imagen_producto_favorito = d.createElement("img");
+  $imagen_producto_favorito.src = producto.url;
+  $imagen_producto_favorito.alt = producto.producto;
+  $contenedor_imagen_producto.appendChild($imagen_producto_favorito);
+
+  let $contenedor_info_favorito = d.createElement("div");
+  $contenedor_info_favorito.classList.add('div-info-favoritos', 'prod-fav');
+  $contenedor_info_favorito.id = "id-info-favoritos";
+  $contenido_producto_favorito.appendChild($contenedor_info_favorito);
+
+  let $contenedor_nombre_precio_producto = d.createElement("div");
+  $contenedor_nombre_precio_producto.classList.add('div-nombre-precio-producto');
+  $contenedor_nombre_precio_producto.id = "id-nombre-precio-producto";
+  $contenedor_nombre_precio_producto.innerHTML = `
+    <span class="span-nombre">${producto.producto}</span>
+    <span class="span-precio">$${producto.precio}</span>
+  `;
+  $contenedor_info_favorito.appendChild($contenedor_nombre_precio_producto);
+
+  let $contenedor_btn_eliminar_producto_favorito = d.createElement("div");
+  $contenedor_btn_eliminar_producto_favorito.classList.add('div-btn-eliminar-producto-favoritos');
+  $contenedor_btn_eliminar_producto_favorito.id = "id-btn-eliminar-producto-favoritos";
+  $contenedor_info_favorito.appendChild($contenedor_btn_eliminar_producto_favorito);
+
+  let $boton_remover_producto = d.createElement("button");
+  $boton_remover_producto.classList.add("btn-remover-producto");
+  $boton_remover_producto.onclick = () => {
+    eliminar_producto_favorito(index);
+  }
+  $boton_remover_producto.innerHTML = `
+    <i class="fas fa-trash" style="color:red";></i>
+  `;
+  $contenedor_btn_eliminar_producto_favorito.appendChild($boton_remover_producto);
+}
+
+// Actualiza el número de productos agregados en el carrito (icono)
+const numero_productos_favoritos = () => {
+  let $numeroIncrementador = d.getElementById("numero-prod-favoritos");
+  if ($numeroIncrementador) {
+    $numeroIncrementador.textContent = productos_favoritos_count;
+  } else {
+    console.warn("Elemento de número de productos favoritos no encontrado.");
+  }
+};
+
+// Elimina el producto de favoritos
+const eliminar_producto_favorito = (index) => {
+  const $productoFavorito = d.getElementById(`id-prod-favorito-${index}`);
+  if ($productoFavorito) {
+    $productoFavorito.remove();
+
+    productos_favoritos_count--;
+    numero_productos_favoritos();
+
+    console.info(`Se ha eliminado de favoritos el Producto con índice [${index}]`);
+  } else {
+    console.warn(`No ha sido posible eliminar el producto con índice [${index}] de favoritos`);
+  }
+};
 
 export default products_json_favoritos;
